879cde30188bfe216e23858517e22479d2597061
Santanu Dey <santanu@KJP-LTP-003.kyosanindia.local>
2025-08-15 14:13:46 +0530

    add file

---
+++ api/angel/GetOpenAPIScripMaster.js
@@ -0,0 +1,13 @@
+import fs from 'fs';
+import path from 'path';
+
+export default function handler(req, res) {
+  const filePath = path.join(process.cwd(), 'api', 'angel', 'OpenAPIScripMaster.json');
+  try {
+    const jsonData = fs.readFileSync(filePath, 'utf8');
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).send(jsonData);
+  } catch (error) {
+    res.status(500).json({ error: 'File not found or cannot be read' });
+  }
+}
